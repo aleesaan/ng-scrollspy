c7658184c78d3b02b874249b2c3c39a031fbc531
Alesandro Santoro <alessandro.santoro@paylogic.eu>
2019-03-25 10:17:22 +0100

    init demo repo

--- projects/ng-scrollspy/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-scrollspy
- */
-
-export * from './lib/scrollspy.service';
-export * from './lib/scrollspy-element.directive';
-export * from './lib/scrollspy.module';
